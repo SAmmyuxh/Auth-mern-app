154e138221b2f1232993add0cb2ccf999dd34b10
SAmmyuxh <149459868+SAmmyuxh@users.noreply.github.com>
2025-01-25 18:44:19 +0530

    Update Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -24,7 +24,7 @@ function Login() {
      return handleerror(" email and password are required")
    }
    try {
-     const url = "http://localhost:8080/auth/login"
+     const url = "https://auth-mern-app-api-ashy.vercel.app/auth/login"
      const response = await fetch(url,
        {
          method:"POST",
